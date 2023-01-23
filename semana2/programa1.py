56ff5bf681d58a12b936e5c3db0fe87e0999fa43
anhuarMI <122800609+anhuar130204@users.noreply.github.com>
2023-01-23 08:54:11 -0600

    actualizacion

--- semana2/programa1.py
+++ semana2/programa1.py
@@ -1,1 +1,11 @@
+"""
+   Programa1
+   Nombre:Anhuar Fernando MI                                              
+   Fecha: 23/01/2023                                                      
+   Descripcion:se realizo y aplico comentarios multilinea y  concatenacion
+"""
+#  las 3 comillas multiples se utilizan para comentarios multilinea
 
+variable1 = "Hola"  #  variable para almacenar una cadena de caracteres
+variable2 = "mundo python"   #  variable para almacenar una cadena de caracteres
+print(variable1 + variable2)

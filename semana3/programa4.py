073af9b55dc43dfa483cd84176cf723be299a25d
anhuarMI <122800609+anhuar130204@users.noreply.github.com>
2023-01-31 02:52:32 +0000

    actualizacion

--- semana3/programa4.py
+++ semana3/programa4.py
@@ -1,12 +1,12 @@
-import math
-numero1 = int(input('Numero 1:'))
-numero2 = int(input("numero 2:"))
-suma = numero1 + numero2
-resta = numero1 - numero2
-multiplicacion = numero1 * numero2
-div = numero1 / numero2
-pot = math.pow(numero1 , numero2)
-print("el resultado de la suma es:",suma,"\n"
+import math #importacion del metodo math
+numero1 = int(input('Numero 1:')) # se declara la variable y pide introducir un entero
+numero2 = int(input("numero 2:")) # se declara la variable y pide introducir un entero
+suma = numero1 + numero2   # se realiza la operacion suma
+resta = numero1 - numero2   # se realiza la operacion resta
+multiplicacion = numero1 * numero2   # se realiza la operacion de multiplicacion
+div = numero1 / numero2   # se realiza la operacion de division
+pot = math.pow(numero1 , numero2)    # se realiza la operacion de potencia con el metodo math
+print("el resultado de la suma es:",suma,"\n"  # se imprime todas las operaciones con salto de linea
       "Resta:",resta,"\n"
       "multiplicacion",multiplicacion,"\n"
       "division",div,"\n"
